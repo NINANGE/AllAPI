f4add69be27423b43ce657ed67e0ae45f9e272ee
NINANGE <m13672456277@163.com>
2017-10-20 16:47:48 +0800

    update AllAPI 4

--- TmallYuShouApp/TmallApiConnectionModel.py
+++ TmallYuShouApp/TmallApiConnectionModel.py
@@ -18,10 +18,10 @@ def GetTmallYuShouBaseInfoData(TreasureID):
 
     if TreasureID == None or len(str(TreasureID))==0:
         print '返回所有数据'
-        result = conn.TmallYuShouDB.TmallYuShouTB.find({})
+        result = conn.TmallYuShouDB.TmallYuShouBaseInfoTB.find({})
     else:
         print '进来了'
-        result = conn.TmallYuShouDB.TmallYuShouTB.find({"TreasureID": {"$in": [TreasureID]}})
+        result = conn.TmallYuShouDB.TmallYuShouBaseInfoTB.find({"TreasureID": {"$in": [TreasureID]}})
 
     print '返回基本信息'
     return result
